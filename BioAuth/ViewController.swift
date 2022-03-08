64feca5490244889272c38273d1e9dc990d3f000
Mariana <mariana@fastmail.net>
2022-03-08 16:09:23 +0100

    evaluating policy

--- BioAuth/ViewController.swift
+++ BioAuth/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    var context = LAContext()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        context.localizedCancelTitle = "End session"
+        context.localizedFallbackTitle = "Use password"
+        context.localizedReason = "The app needs your authentication"
+        evaluatePolicy()
     }
 
-
+    func evaluatePolicy() {
+        var errorCanEval: NSError?
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &errorCanEval) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Fallback title - override reason") { success, error in
+                print(success)
+                if let err = error {
+                    let evalErrCode = LAError(_nsError: err as NSError)
+                    switch evalErrCode.code {
+                    case LAError.Code.userCancel:
+                        print("user cancelled")
+                    case LAError.Code.userFallback:
+                        print("fallback")
+                    case LAError.Code.authenticationFailed:
+                        print("failed")
+                    default:
+                        print("other error")
+                    }
+                }
+            }
+        } else {
+            print("can't evaluate")
+            print(errorCanEval?.localizedDescription ?? "no error desc")
+        }
+    }
 }
 
+// evaluating policy
